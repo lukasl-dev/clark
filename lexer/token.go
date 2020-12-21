cf2e0c0916c42e06408177eb38f07d88b1118224
lukasl-dev <git@lukasl.dev>
2020-12-21 16:02:43 +0100

    Add Token in token.go

--- lexer/token.go
+++ lexer/token.go
@@ -1,5 +1,10 @@
 package lexer
 
+import (
+	"fmt"
+	"log"
+)
+
 type TokenType int
 
 const (
@@ -11,3 +16,32 @@ const (
 	TokenTypeText
 	TokenTypeFlagName
 )
+
+var tokenMap = map[TokenType]string{
+	TokenTypeEOF:     "EOF",
+	TokenTypeIllegal: "Illegal",
+
+	TokenTypePrefix:   "TokenPrefix",
+	TokenTypeLabel:    "TokenLabel",
+	TokenTypeText:     "TokenText",
+	TokenTypeFlagName: "TokenFlagName",
+}
+
+type Token struct {
+	Type  TokenType `json:"type,omitempty"`
+	Value string    `json:"value,omitempty"`
+}
+
+func NewToken(tokenType TokenType, value interface{}) Token {
+	return Token{Type: tokenType, Value: fmt.Sprint(value)}
+}
+
+func (t Token) String() string {
+	s, ok := tokenMap[t.Type]
+
+	if !ok {
+		log.Panicf("missing string representation for %d", t.Type)
+	}
+
+	return fmt.Sprintf("%s: '%s'", s, t.Value)
+}
