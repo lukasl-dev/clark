0ec93a3c78b05bf0f2d828e537069f0caf2ba2de
lukasl-dev <git@lukasl.dev>
2020-12-23 18:05:10 +0100

    Add usages of flags

--- cmd/flags.go
+++ cmd/flags.go
@@ -21,19 +21,19 @@ var (
 )
 
 func defineFlags(set *pflag.FlagSet) {
-	set.StringVarP(&input, "input", "i", string(readPipe(150)), "")
+	set.StringVarP(&input, "input", "i", string(readPipe(150)), "define raw input")
 
 	/* prefix */
-	set.StringSliceVarP(&prefixes, "prefix", "p", []string{}, "")
-	set.BoolVar(&prefixIgnoreCase, "prefix-ignore-case", false, "")
+	set.StringSliceVarP(&prefixes, "prefix", "p", []string{}, "add a prefix")
+	set.BoolVar(&prefixIgnoreCase, "prefix-ignore-case", false, "set prefix matching to ignore-case")
 
 	/* label */
-	set.StringSliceVarP(&labels, "label", "l", []string{}, "")
-	set.BoolVar(&labelIgnoreCase, "label-ignore-case", false, "")
+	set.StringSliceVarP(&labels, "label", "l", []string{}, "add a label")
+	set.BoolVar(&labelIgnoreCase, "label-ignore-case", false, "set label matching to ignore-case")
 
 	/* miscellaneous */
-	set.BoolVarP(&advanced, "advanced", "a", false, "")
-	set.BoolVar(&prettyPrint, "pretty-print", false, "")
+	set.BoolVarP(&advanced, "advanced", "a", false, "set advanced lookup")
+	set.BoolVar(&prettyPrint, "pretty-print", false, "set json pretty-print")
 }
 
 func validateFlags() error {
