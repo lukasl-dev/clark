2c55cf8babfb546e569838ecd531e40a72ca90e4
lukasl-dev <git@lukasl.dev>
2021-07-04 18:03:14 +0200

    feat: add TokenizerOptions

---
+++ tokenizer_options.go
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 lukas.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package clark
+
+type TokenizerOptions struct {
+	prefixes         []string
+	prefixIgnoreCase bool
+	labels           []string
+	labelIgnoreCase  bool
+}
+
+func NewTokenizerOptions() *TokenizerOptions {
+	return &TokenizerOptions{}
+}
+
+func minimizeTokenizerOptions(opts []*TokenizerOptions) *TokenizerOptions {
+	if len(opts) > 0 {
+		return opts[0]
+	}
+	return NewTokenizerOptions()
+}
+
+func (opts *TokenizerOptions) WithPrefixes(prefixes ...string) *TokenizerOptions {
+	opts.prefixes = prefixes
+	return opts
+}
+
+func (opts *TokenizerOptions) WithPrefixIgnoreCase(prefixIgnoreCase bool) *TokenizerOptions {
+	opts.prefixIgnoreCase = prefixIgnoreCase
+	return opts
+}
+
+func (opts *TokenizerOptions) WithLabels(labels ...string) *TokenizerOptions {
+	opts.labels = labels
+	return opts
+}
+
+func (opts *TokenizerOptions) WithLabelIgnoreCase(labelIgnoreCase bool) *TokenizerOptions {
+	opts.labelIgnoreCase = labelIgnoreCase
+	return opts
+}
---
+++ tokenizer_options_test.go
@@ -0,0 +1,81 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 lukas.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package clark
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTokenizerOptions(t *testing.T) {
+	actual := NewTokenizerOptions()
+	expected := new(TokenizerOptions)
+	assert.Equal(t, expected, actual)
+}
+
+func TestTokenizerOptions_WithPrefixes(t *testing.T) {
+	var (
+		prefixes = []string{"/", "-", "!"}
+	)
+	actual := NewTokenizerOptions().WithPrefixes(prefixes...)
+	expected := &TokenizerOptions{
+		prefixes: prefixes,
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestTokenizerOptions_WithPrefixIgnoreCase(t *testing.T) {
+	const (
+		prefixIgnoreCase = true
+	)
+	actual := NewTokenizerOptions().WithPrefixIgnoreCase(prefixIgnoreCase)
+	expected := &TokenizerOptions{
+		prefixIgnoreCase: prefixIgnoreCase,
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestTokenizerOptions_WithLabels(t *testing.T) {
+	var (
+		labels = []string{"play", "resume", "stop", "skip"}
+	)
+	actual := NewTokenizerOptions().WithLabels(labels...)
+	expected := &TokenizerOptions{
+		labels: labels,
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestTokenizerOptions_WithLabelIgnoreCase(t *testing.T) {
+	const (
+		labelIgnoreCase = true
+	)
+	actual := NewTokenizerOptions().WithLabelIgnoreCase(labelIgnoreCase)
+	expected := &TokenizerOptions{
+		labelIgnoreCase: labelIgnoreCase,
+	}
+	assert.Equal(t, expected, actual)
+}
